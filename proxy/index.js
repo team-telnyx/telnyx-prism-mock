ab73df50e0203bf962fac23061b1e980d14cf83a
Drew Kitch <drew@telnyx.com>
2021-12-16 23:35:45 -0700

    lint fix (#2)

--- proxy/index.js
+++ proxy/index.js
@@ -1,32 +1,29 @@
 const express = require('express');
-const morgan = require("morgan");
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const morgan = require('morgan');
+const {createProxyMiddleware} = require('http-proxy-middleware');
 
 // Create Express Server
 const app = express();
 
 // Configuration
 const PORT = 12111;
-const HOST = "0.0.0.0";
-const API_SERVICE_URL = "http://0.0.0.0:4010";
+const HOST = '0.0.0.0';
+const API_SERVICE_URL = 'http://0.0.0.0:4010';
 
 // Logging
 app.use(morgan('combined'));
 
 // Proxy endpoints
 const proxyMiddleware = createProxyMiddleware({
-    target: API_SERVICE_URL,
-    changeOrigin: true,
-    pathRewrite: {
-        [`^/v2/`]: '/',
-    },
-    logLevel: 'debug'
+  target: API_SERVICE_URL,
+  changeOrigin: true,
+  pathRewrite: {
+      [`^/v2/`]: '/',
+  },
+  logLevel: 'debug'
 });
 
 app.use('', proxyMiddleware);
 
 // Start the Proxy
-app.listen(PORT, HOST, () => {
-    console.log(`Starting Proxy at ${HOST}:${PORT}`);
- });
-
+app.listen(PORT, HOST);
